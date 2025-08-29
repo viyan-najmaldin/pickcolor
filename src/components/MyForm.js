aabd65852423a5e5a0011d5f58c11abb54c07ea1
viyan-najmaldin <najmadinviyan@gmail.com>
2025-08-29 16:41:28 +0300

    remove the button

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -2,16 +2,13 @@ import '../App.css';
 
 const MyForm = ({setColor}) => {
   return (
-  <form onSubmit={(e)=>{
-            e.preventDefault()  
-          setColor(e.target.viyan.value)
-            }}>
+          <>
                 <input type="text"
                 placeholder="Write color name or hexa value"
-                name="viyan"
+                // name="viyan"
+                onChange={ (e) =>  setColor(e.target.value)}
                 />
-                <button type="submit"> Add </button>
-          </form>
+          </>
   )
 }
 
